296f910e5f1d899aede6432959723f511d0d3aa2
Kirill Kucher <kvkucher@avito.ru>
2024-06-06 20:17:12 +0300

    add service tests

--- grpc_service/main.go
+++ grpc_service/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 	"log"
 	"net"
@@ -10,169 +8,15 @@ import (
 
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+
+	"grpc_service/server"
 
 	pb "grpc_service/proto"
-
-	"common"
 )
-
-var db *sql.DB
-
-func connectDB() (*sql.DB, error) {
-	connStr := os.Getenv("DATA_SOURCE")
-
-	db, err := sql.Open("postgres", connStr)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-
-	_, err = db.Exec(`
-	CREATE TABLE IF NOT EXISTS tasks (
-		id SERIAL PRIMARY KEY,
-		title TEXT,
-		content TEXT,
-		createdBy INT
-	);
-	`)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-type server struct {
-	pb.UnimplementedTaskServiceServer
-}
-
-func (s *server) CreateTask(ctx context.Context, req *pb.CreateTaskRequest) (*pb.Task, error) {
-	userID, err := common.Authenticate(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	var taskID int32
-	err = db.QueryRowContext(ctx, "INSERT INTO tasks (title, content, createdBy) VALUES ($1, $2, $3) RETURNING id", req.Title, req.Content, userID).Scan(&taskID)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to create task: %v", err)
-	}
-
-	return &pb.Task{
-		Id:              taskID,
-		Title:           req.Title,
-		Content:         req.Content,
-		CreatedByUserID: userID,
-	}, nil
-}
-
-func (s *server) UpdateTask(ctx context.Context, req *pb.UpdateTaskRequest) (*pb.Task, error) {
-	userID, err := common.Authenticate(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := db.ExecContext(ctx, "UPDATE tasks SET title = $1, content = $2 WHERE id = $3 AND createdBy = $4", req.Title, req.Content, req.TaskId, userID)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to update task: %v", err)
-	}
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to update task: %v", err)
-	}
-
-	if rowsAffected == 0 {
-		return nil, status.Errorf(codes.PermissionDenied, "Access denied")
-	}
-
-	return &pb.Task{
-		Id:              req.TaskId,
-		Title:           req.Title,
-		Content:         req.Content,
-		CreatedByUserID: userID,
-	}, nil
-}
-
-func (s *server) DeleteTask(ctx context.Context, req *pb.DeleteTaskRequest) (*pb.DeleteTaskResponse, error) {
-	userID, err := common.Authenticate(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := db.ExecContext(ctx, "DELETE FROM tasks WHERE id = $1 AND createdBy = $2", req.TaskId, userID)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to update task: %v", err)
-	}
-
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to delete task: %v", err)
-	}
-
-	if rowsAffected == 0 {
-		result, _ := db.ExecContext(ctx, "SELECT id FROM tasks WHERE id = $1", req.TaskId)
-		rowsAffected, _ := result.RowsAffected()
-		if rowsAffected != 0 {
-			return nil, status.Errorf(codes.PermissionDenied, "Access denied")
-		}
-		return nil, status.Errorf(codes.NotFound, "task not found")
-	}
-
-	return &pb.DeleteTaskResponse{
-		Success: true,
-	}, nil
-}
-
-func (s *server) GetTaskById(ctx context.Context, req *pb.GetTaskByIdRequest) (*pb.Task, error) {
-	_, err := common.Authenticate(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	var task pb.Task
-	err = db.QueryRowContext(ctx, "SELECT id, title, content, createdBy FROM tasks WHERE id = $1", req.TaskId).Scan(&task.Id, &task.Title, &task.Content, &task.CreatedByUserID)
-	if err != nil {
-		return nil, status.Errorf(codes.NotFound, "task not found")
-	}
-
-	return &task, nil
-}
-
-func (s *server) GetTaskListWithPagination(ctx context.Context, req *pb.GetTaskListRequest) (*pb.GetTaskListResponse, error) {
-	rows, err := db.QueryContext(ctx, "SELECT id, title, content FROM tasks LIMIT $1 OFFSET $2", req.PageSize, req.PageNumber*req.PageSize)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to fetch tasks: %v", err)
-	}
-	defer rows.Close()
-
-	var tasks []*pb.Task
-	for rows.Next() {
-		var task pb.Task
-		if err := rows.Scan(&task.Id, &task.Title, &task.Content); err != nil {
-			return nil, status.Errorf(codes.Internal, "failed to scan task: %v", err)
-		}
-		tasks = append(tasks, &task)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, status.Errorf(codes.Internal, "error iterating over rows: %v", err)
-	}
-
-	return &pb.GetTaskListResponse{
-		Tasks: tasks,
-	}, nil
-}
 
 func main() {
 	var err error
-	db, err = connectDB()
+	db, err := server.ConnectDB(os.Getenv("DATA_SOURCE"))
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
@@ -184,7 +28,9 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterTaskServiceServer(s, &server{})
+	pb.RegisterTaskServiceServer(s, &server.Server{
+		Db: db,
+	})
 	fmt.Println("Server running on port :50051")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
--- statistics_service/go.mod
+++ statistics_service/go.mod
@@ -6,11 +6,50 @@ require github.com/ClickHouse/clickhouse-go/v2 v2.24.0
 
 require (
 	common v0.0.0-00010101000000-000000000000 // indirect
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/Microsoft/hcsshim v0.11.4 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/containerd/containerd v1.7.15 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/cpuguy83/dockercfg v0.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/distribution/reference v0.5.0 // indirect
+	github.com/docker/docker v26.1.1+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.5.0 // indirect
+	github.com/moby/sys/user v0.1.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
+	go.opentelemetry.io/otel/metric v1.26.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/mod v0.16.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 )
@@ -20,6 +59,7 @@ require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/go-faster/city v1.0.1 // indirect
 	github.com/go-faster/errors v0.7.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/paulmach/orb v0.11.1 // indirect
@@ -28,9 +68,11 @@ require (
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/stretchr/testify v1.9.0
+	github.com/testcontainers/testcontainers-go v0.31.0
+	github.com/testcontainers/testcontainers-go/modules/clickhouse v0.31.0
 	go.opentelemetry.io/otel v1.26.0 // indirect
 	go.opentelemetry.io/otel/trace v1.26.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	google.golang.org/grpc v1.64.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
